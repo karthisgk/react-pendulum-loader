95ca596eb97b7e88da1e89fd51abf2155e3613cd
karthickdotsonniyagokulnath <100826547+karthickdotsonniyagokulnath@users.noreply.github.com>
2022-08-23 20:57:34 +0530

    inital version

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+// import "./style.css"
+
+interface LoaderProps {
+    duration: string;
+    dotColor: string;
+    backgroundDim: number;
+    show: boolean;
+}
+
+function Loader({duration, show, dotColor, backgroundDim}: LoaderProps) {
+    return (
+        <div className={`loader ${show ? "show" : ""}`}>
+            <div className="dot-holder">
+                <span className="dot"></span>
+                <div className="dots">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader

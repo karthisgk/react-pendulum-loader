f3985642af3f46ebe0a8b0bf364f7e8d594d8d18
karthickdotsonniyagokulnath <100826547+karthickdotsonniyagokulnath@users.noreply.github.com>
2022-08-23 21:26:21 +0530

    add custom css var

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -10,7 +10,11 @@ interface LoaderProps {
 
 function Loader({duration, show, dotColor, backgroundDim}: LoaderProps) {
     return (
-        <div className={`loader ${show ? "show" : ""}`}>
+        <div className={`loader ${show ? "show" : ""}`} style={{
+            "--animate-deplay": duration || "1.8s",
+            "--dot-color": dotColor || "rgb(134 140 249)",
+            "--bg-dim": backgroundDim || .7
+        } as React.CSSProperties}>
             <div className="dot-holder">
                 <span className="dot"></span>
                 <div className="dots">
